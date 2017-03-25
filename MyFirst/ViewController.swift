da27594d9e2f278a326e85b3c8a06c8f57b5f8c9
takashi fujikawa <misumame@icloud.com>
2017-03-25 22:23:50 +0900

    Initial Commit

---
+++ MyFirst/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirst
+//
+//  Created by 藤川 貴史 on 2017/03/25.
+//  Copyright © 2017年 みすだまめお. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
